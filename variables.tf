37ff3394e4aa5689e4cb55a047f48cbe24004105
Yura <shupik3@gmail.com>
2023-07-12 13:48:44 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  type    = string
+  default = "AKIA3JWEQ5JLXR5MPQNZ"
+}
+
+variable "aws_secret_key" {
+  type    = string
+  default = "6vcaUdNY/S7c+Q0Lho8xUhgEZt9kzbdKWyG6ZQd8"
+}
