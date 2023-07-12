762433afee45e12b654270824a4cb6323b2fc01c
Yura <shupik3@gmail.com>
2023-07-12 18:40:19 +0300

    Create EC2-Instance-function-ports.tf

---
+++ EC2-Instance-function-ports.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "EC2-Instance" {
+  availability_zone      = "eu-north-1a"
+  ami                    = "ami-0989fb15ce71ba39e"
+  instance_type          = "t3.micro"
+  key_name               = "sv111"
+  vpc_security_group_ids = [aws_security_group.DefaultTerraformSG.id]
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 10
+    volume_type = "standard"
+    tags = {
+      Name = "root-disk"
+    }
+  }
+
+  user_data = file("files/install.sh")
+}
+
+resource "aws_security_group" "DefaultTerraformSG" {
+  name        = "DefaultTerraformSG"
+  description = "Allow 22, 80, 443 inbound taffic"
+
+  dynamic "ingress" {
+    for_each = ["22", "80", "443", "8080", "8000"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
